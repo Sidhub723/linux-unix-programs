97f6f594cd03ac5c931affe532ddc91dfec2c995
Sidhub723 <sidbhat.211ee151@nitk.edu.in>
2023-07-12 19:37:33 +0530

    Added support to show time taken for execution and real time elapsed

--- Week1/mycat.c
+++ Week1/mycat.c
@@ -1,21 +1,36 @@
-/*Using unbuffered IO => done using userspace library functions
-  Preferably compile using flags --Wall, --Werror and --Wextra
-  To cat a file, use the redirect the file's FD using '<' from your shell
+/*
+* Using unbuffered IO => done using userspace library functions
+* Preferably compile using flags --Wall, --Werror and --Wextra
+* To cat a file, use the redirect the file's FD using '<' from your shell
+* clock_gettime() may not work in some scenarios. Use -lrt flags in case of gcc compiler.
+* Exit using Ctrl + D or Ctrl + C
 */
 
 
 #include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #define SIZE 512
+double fetch_time() {
+    struct timespec current;
 
+    if(clock_gettime(CLOCK_REALTIME, &current) == -1) {
+        fprintf(stderr,"ERROR!Unable to access time: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    return current.tv_sec + current.tv_nsec*1e-9;
+
+}
 int main(int argc, char **argv) {
+    double start_time = fetch_time();
+    clock_t cpu_start_time = clock(); 
     char buf[SIZE];
     int n;
-    
+    printf("Exit using Crtl + D or Ctrl + C");
     /*argc and argv are casted simply, to prevent compiler warnings of
     unused variables lol */
     (void)argc;
@@ -32,6 +47,9 @@ int main(int argc, char **argv) {
         fprintf(stderr,"ERROR!Unable to read: %s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
+    printf("Real(astronomical) time taken is %.6lf\n",fetch_time()-start_time);
+    clock_t cpu_end_time = clock();
+    printf("CPU clock time taken is %.9lu\n",((cpu_end_time - cpu_start_time)*1000)/CLOCKS_PER_SEC);
     return (EXIT_SUCCESS);
 
 }
